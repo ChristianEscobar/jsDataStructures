ef290c7a3a787d734f48f471c1a97100fdad6548
Christian Escobar <crs.escobar@gmail.com>
2019-06-22 23:21:45 -0700

    add more tests

--- linked-list/tests/linked-list.test.js
+++ linked-list/tests/linked-list.test.js
@@ -11,7 +11,7 @@ test('Adds a node to the list', () => {
 	expect(linkedList.size()).toBe(1);
 });
 
-test('Adds a node as the first', () => {
+test('Adds new node as the first', () => {
 	let result = 44;
 	const linkedList = new LinkedList();
 	linkedList.add(11);
@@ -19,4 +19,76 @@ test('Adds a node as the first', () => {
 	linkedList.add(33);
 	linkedList.addFirst(44);
 	expect(linkedList.get(0)).toBe(44);
+});
+
+test('Adds new node as the last', () => {
+	let result = 44;
+	const linkedList = new LinkedList();
+	linkedList.add(11);
+	linkedList.add(22);
+	linkedList.add(33);
+	linkedList.addLast(44);
+	expect(linkedList.get(3)).toBe(44);
+});
+
+test('Clears a list', () => {
+	const linkedList = new LinkedList();
+	linkedList.add(11);
+	linkedList.add(22);
+	linkedList.add(33);
+	linkedList.add(44);
+	linkedList.clear();
+	expect(linkedList.size()).toBe(0);
+});
+
+test('Checks if list contains a specific value', () => {
+	const linkedList = new LinkedList();
+	linkedList.add(11);
+	linkedList.add(22);
+	linkedList.add(33);
+	linkedList.add(44);
+	linkedList.add(55);
+	expect(linkedList.contains(33)).toBeTruthy();
+});
+
+test('Checks if list does not contain a specific value', () => {
+	const linkedList = new LinkedList();
+	linkedList.add(11);
+	linkedList.add(22);
+	linkedList.add(33);
+	linkedList.add(44);
+	linkedList.add(55);
+	expect(linkedList.contains(77)).toBeFalsy();
+});
+
+test('Gets value from a specific index', () => {
+	const linkedList = new LinkedList();
+	linkedList.add(11);
+	linkedList.add(22);
+	linkedList.add(33);
+	linkedList.add(44);
+	linkedList.add(55);
+	expect(linkedList.get(3)).toBe(44);
+});
+
+test('Removes node from a list', () => {
+	const linkedList = new LinkedList();
+	linkedList.add(11);
+	linkedList.add(22);
+	linkedList.add(33);
+	linkedList.add(44);
+	linkedList.add(55);
+	linkedList.remove(22);
+	expect(linkedList.contains(22)).toBeFalsy();
+});
+
+test('Gets the size of a list', () => {
+	const linkedList = new LinkedList();
+	linkedList.add(11);
+	linkedList.add(22);
+	linkedList.add(33);
+	linkedList.add(44);
+	linkedList.add(55);
+	linkedList.add(66);
+	expect(linkedList.size()).toBe(6);
 });

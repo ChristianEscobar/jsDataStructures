b0d6f580c0a33cd4b2847b24b30fdf4101214842
Christian Escobar <crs.escobar@gmail.com>
2019-06-24 21:33:57 -0700

    refactor tests

--- linked-list/tests/linked-list.test.js
+++ linked-list/tests/linked-list.test.js
@@ -1,94 +1,131 @@
 const LinkedList = require('../LinkedList');
 
-test('Creates a new linked list', () => {
-	const linkedList = new LinkedList();
-	expect(linkedList.size()).toBe(0);
-});
+describe('Linked List', () => {
+	test('Create new', () => {
+		const linkedList = new LinkedList();
+		expect(linkedList.size()).toBe(0);
+	});
 
-test('Adds a node to the list', () => {
-	const linkedList = new LinkedList();
-	linkedList.add(11);
-	expect(linkedList.size()).toBe(1);
-});
+	test('Add node', () => {
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		expect(linkedList.size()).toBe(1);
+	});
 
-test('Adds new node as the first', () => {
-	let result = 44;
-	const linkedList = new LinkedList();
-	linkedList.add(11);
-	linkedList.add(22);
-	linkedList.add(33);
-	linkedList.addFirst(44);
-	expect(linkedList.get(0)).toBe(44);
-});
+	test('Add node as first', () => {
+		let result = 44;
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.addFirst(44);
+		expect(linkedList.get(0)).toBe(44);
+	});
 
-test('Adds new node as the last', () => {
-	let result = 44;
-	const linkedList = new LinkedList();
-	linkedList.add(11);
-	linkedList.add(22);
-	linkedList.add(33);
-	linkedList.addLast(44);
-	expect(linkedList.get(3)).toBe(44);
-});
+	test('Add node as last', () => {
+		let result = 44;
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.addLast(44);
+		expect(linkedList.get(3)).toBe(44);
+	});
 
-test('Clears a list', () => {
-	const linkedList = new LinkedList();
-	linkedList.add(11);
-	linkedList.add(22);
-	linkedList.add(33);
-	linkedList.add(44);
-	linkedList.clear();
-	expect(linkedList.size()).toBe(0);
-});
+	test('Clear list', () => {
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.add(44);
+		linkedList.clear();
+		expect(linkedList.size()).toBe(0);
+	});
 
-test('Checks if list contains a specific value', () => {
-	const linkedList = new LinkedList();
-	linkedList.add(11);
-	linkedList.add(22);
-	linkedList.add(33);
-	linkedList.add(44);
-	linkedList.add(55);
-	expect(linkedList.contains(33)).toBeTruthy();
-});
+	test('Contains a specific value', () => {
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.add(44);
+		linkedList.add(55);
+		expect(linkedList.contains(33)).toBeTruthy();
+	});
 
-test('Checks if list does not contain a specific value', () => {
-	const linkedList = new LinkedList();
-	linkedList.add(11);
-	linkedList.add(22);
-	linkedList.add(33);
-	linkedList.add(44);
-	linkedList.add(55);
-	expect(linkedList.contains(77)).toBeFalsy();
-});
+	test('Does not contain a specific value', () => {
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.add(44);
+		linkedList.add(55);
+		expect(linkedList.contains(77)).toBeFalsy();
+	});
 
-test('Gets value from a specific index', () => {
-	const linkedList = new LinkedList();
-	linkedList.add(11);
-	linkedList.add(22);
-	linkedList.add(33);
-	linkedList.add(44);
-	linkedList.add(55);
-	expect(linkedList.get(3)).toBe(44);
-});
+	test('Gets value from a specific index', () => {
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.add(44);
+		linkedList.add(55);
+		expect(linkedList.get(3)).toBe(44);
+	});
 
-test('Removes node from a list', () => {
-	const linkedList = new LinkedList();
-	linkedList.add(11);
-	linkedList.add(22);
-	linkedList.add(33);
-	linkedList.add(44);
-	linkedList.add(55);
-	linkedList.remove(22);
-	expect(linkedList.contains(22)).toBeFalsy();
-});
+	test('Removes node', () => {
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.add(44);
+		linkedList.add(55);
+		linkedList.remove(22);
+		expect(linkedList.contains(22)).toBeFalsy();
+	});
 
-test('Gets the size of a list', () => {
-	const linkedList = new LinkedList();
-	linkedList.add(11);
-	linkedList.add(22);
-	linkedList.add(33);
-	linkedList.add(44);
-	linkedList.add(55);
-	linkedList.add(66);
-	expect(linkedList.size()).toBe(6);
+	test('Gets the size of a list', () => {
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.add(44);
+		linkedList.add(55);
+		linkedList.add(66);
+		expect(linkedList.size()).toBe(6);
+	});
+
+	test('Gets the first node in the list', () => {
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.add(44);
+		linkedList.add(55);
+		linkedList.add(66);
+		expect(linkedList.getFirst()).toBe(11);
+	});
+
+	test('Gets the last node in the list', () => {
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.add(44);
+		linkedList.add(55);
+		linkedList.add(66);
+		expect(linkedList.getLast()).toBe(66);
+	});
+
+	test('Reverses a list', () => {
+		const result = '66,55,44,33,22,11';
+		const linkedList = new LinkedList();
+		linkedList.add(11);
+		linkedList.add(22);
+		linkedList.add(33);
+		linkedList.add(44);
+		linkedList.add(55);
+		linkedList.add(66);
+		linkedList.reverse();
+		expect(linkedList.toArray().join(',')).toBe(result);
+	});
 });

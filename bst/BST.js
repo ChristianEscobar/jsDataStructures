5f9a0046dbec7d89b868b49c4a9876109be49bd6
Christian Escobar <crs.escobar@gmail.com>
2019-06-28 21:43:07 -0700

    binary search tree initial commit

---
+++ bst/BST.js
@@ -0,0 +1,44 @@
+const Node = require('./Node');
+
+class Bst {
+	constructor() {
+		this.root = null;
+	}
+
+	add(data) {
+		const traverse = function(data, curNode) {
+			if (data < curNode.data) {
+				if (!curNode.left) {
+					const newNode = new Node(data);
+					curNode.left = newNode;
+					return;
+				} else {
+					// Go left
+					return traverse(data, curNode.left);
+				}
+			} else if (data > curNode.data) {
+				if (!curNode.right) {
+					const newNode = new Node(data);
+					curNode.right = newNode;
+					return;
+				} else {
+					// Go right
+					return traverse(data, curNode.right);
+				}
+			}
+		};
+
+		if (!this.root) {
+			const node = new Node(data);
+			this.root = node;
+		} else {
+			traverse(data, this.root);
+		}
+	}
+
+	print() {
+		console.log(JSON.parse(JSON.stringify(this.root)));
+	}
+}
+
+module.exports = Bst;
--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const LinkedList = require('./linked-list/LinkedList');
+const Bst = require('./bst/BST');
 
-const linkedList = new LinkedList();
-linkedList.add(11);
-linkedList.add(22);
-linkedList.add(33);
-linkedList.add(44);
-linkedList.add(55);
+const bst = new Bst();
+bst.add(7);
+bst.add(5);
+bst.add(10);
+bst.add(2);
+bst.add(8);
+bst.add(11);
+bst.print();
+// const linkedList = new LinkedList();
+// linkedList.add(11);
+// linkedList.add(22);
+// linkedList.add(33);
+// linkedList.add(44);
+// linkedList.add(55);
 
-//linkedList.print();
+// //linkedList.print();
 
-linkedList.remove(11);
-linkedList.addFirst(66);
-linkedList.addLast(77);
+// linkedList.remove(11);
+// linkedList.addFirst(66);
+// linkedList.addLast(77);
 
-linkedList.print();
-//console.log(linkedList.size());
-//console.log(linkedList.contains(33));
-//console.log(linkedList.contains(100));
-// console.log(linkedList.get(0));
-// console.log(linkedList.get(3));
-//console.log(linkedList.getFirst());
-linkedList.reverse();
-console.log(linkedList.toArray().join(','));
+// linkedList.print();
+// //console.log(linkedList.size());
+// //console.log(linkedList.contains(33));
+// //console.log(linkedList.contains(100));
+// // console.log(linkedList.get(0));
+// // console.log(linkedList.get(3));
+// //console.log(linkedList.getFirst());
+// linkedList.reverse();
+// console.log(linkedList.toArray().join(','));
